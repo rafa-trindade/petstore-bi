074af1fe367b2d54754822c3cf9de08b45070443
Rafael Trindade <rafatrindade.exe@gmail.com>
2025-10-06 11:00:26 -0300

    Initial commit

--- data/extract/extract_to_parquet.py
+++ data/extract/extract_to_parquet.py
@@ -1,16 +1,21 @@
 import pandas as pd
 from config.db_config import engine
+from datetime import datetime
 
 CSV_PATH = "data/lojas.parquet"
 
-
 def extract_to_parquet():
-    
+
     query = "SELECT * FROM lojas_gold;"
     df_lojas = pd.read_sql(query, engine)
+
+    if 'data_extracao' in df_lojas.columns:
+        df_lojas['data_extracao'] = pd.to_datetime(df_lojas['data_extracao'], errors='coerce').dt.strftime('%d/%m/%Y')
+
+    df_lojas['ultima_atualizacao'] = datetime.today().strftime('%d/%m/%Y')
 
     df_lojas.to_parquet(CSV_PATH, index=False)
 
     print(f"{len(df_lojas)} registros salvos em {CSV_PATH}")
-    
+
     return df_lojas
